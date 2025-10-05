9da1cf8c68040190268fc70ca8d871582ed9bca7
tharun_raj <tharunraj.248@gmail.com>
2025-10-05 21:43:41 +0530

    implemented redis from external

--- planets/config/redis_config.py
+++ planets/config/redis_config.py
@@ -8,13 +8,7 @@ load_dotenv()
 redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 
-r = redis.Redis(
-    host=redis_host, 
-    port=redis_port, 
-    decode_responses=False,
-    socket_connect_timeout=5,
-    socket_timeout=5
-)
+r = redis.Redis.from_url(redis_host)
 
 def test_redis_connection():
     try:
